f50413b565077e766964b35ede46f4b06541a547
Gillesedgy <edgygilles@pursuit.org>
2022-09-08 23:29:38 -0400

    created a sample proposal

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
-const Base_URL = ('')
-fetch(Based_URL)
-.then(res)
-.then((res.Json)={
+// const userInp = input
+// const Base_URL = "www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
 
-})
-.catch((err)=>{
-    console.log(err)
-})
+// fetch(Base_URL)
+//   .then((res) => res.json())
+//   .then((resJson) => console.log(resJson))
+//   .catch((err) => console.log(err));
+
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "b5b93f8d91mshbf7752d57342d87p1ff623jsn8aa205648c63",
+//     "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
+//   },
+// };
+
+// fetch("https://calorieninjas.p.rapidapi.com/v1/nutrition?query=pizza", options)
+//   .then((response) => response.json())
+//   .then((resjson) => {
+//     console.log(resjson);
+
+//     console.log(resjson.items[0].name);
+//     console.log(resjson.items[0].calories);
+//     console.log(resjson.items[0].sugar_g);
+//     console.log(resjson.items[0].protein_g);
+//   })
+//   .catch((err) => console.error(err));
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "b5b93f8d91mshbf7752d57342d87p1ff623jsn8aa205648c63",
+//     "X-RapidAPI-Host": "montanaflynn-geocoder.p.rapidapi.com",
+//   },
+// };
+
+// fetch(
+//   "https://montanaflynn-geocoder.p.rapidapi.com/address?address=2802%20Clarendon%20rd%2C%20Brooklyn%2C%20NY%20%2C%2011226",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b5b93f8d91mshbf7752d57342d87p1ff623jsn8aa205648c63",
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+  },
+};
+
+fetch(
+  "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=pizza",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
--- proposal.md
+++ proposal.md
@@ -1,3 +1,7 @@
 # Frontend Profolio Project 1
 
 ## User Stories
+
+As the creator
+I want the user to use my application
+So that they can have access to quick easy, simple recipes
