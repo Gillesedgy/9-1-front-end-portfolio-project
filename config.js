c82ca3d9968b9b5bdfcaea4af251fb68ec87c8b4
Gillesedgy <edgygilles@pursuit.org>
2022-09-15 11:13:23 -0400

    Hid API Key

---
+++ config.js
@@ -0,0 +1,7 @@
+// const options = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "b5b93f8d91mshbf7752d57342d87p1ff623jsn8aa205648c63",
+//       "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+//     },
+//   };
